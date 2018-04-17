7050e463530df5f57cdc5c76da394d873903a8a3
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2018-04-17 16:11:06 +0300

    Added all required packages

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ const express = require('express');
 
 const app = express();
 
-// Custom middleware
-const headers = require('./middlewares/headers-config');
-const notFound = require('./middlewares/not-found');
-
 // Setup middlewares
 app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
-    res.json({hello: 'world'})
+    res.json({});
 });
 
 module.exports = app;
